73f82a28d63f366ebc9999fd304541c08e44e75d
unknown <ayushnabar@outlook.com>
2020-08-11 19:58:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = '';
+
+
+bot.on('ready', () => {
+    console.log('This bot is online!');
+})
+
+bot.on('message', msg=>{
+    if(msg.content === "HELLO"){
+        msg.reply('HELLO FRIEND!');
+    }
+})
+bot.login(token);
+
+
