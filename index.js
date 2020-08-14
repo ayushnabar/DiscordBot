f26c7a4bc05e6f87e3e83bc689e02815bd79e520
unknown <ayushnabar@outlook.com>
2020-08-13 19:07:37 -0700

    bot replies

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = '';
+const token = 'NzQyOTM1Mjc1MzAyNDIwNTUw.XzNWZA.ojcfpKTRDOzl0zHbUvbMlYXFH6I';
 
+const PREFIX = "!";
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 bot.on('ready', () => {
     console.log('This bot is online!');
 })
 
-bot.on('message', msg=>{
-    if(msg.content === "HELLO"){
-        msg.reply('HELLO FRIEND!');
+bot.on('message', message=>{
+   let args = message.content.substring(PREFIX.length).split(" ");
+   var num = getRandomInt(3);
+   if (message.author.bot) return;
+   switch(num){
+        case 0:
+            message.channel.send(message.author.username + ' is tilted');
+            break;
+        case 1:
+            message.channel.send(message.author.username + ' is malding');
+            break;
+        case 2:
+            message.channel.send(message.author.username + ' is uninstalling');
+            break;
+        default:
+            message.channel.send('Yeahyuh');
     }
+
 })
+
 bot.login(token);
 
 
